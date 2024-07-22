73b74691308f2d9619ea4b2179c5251376cc530a
vbevz <122693446+vladbevz@users.noreply.github.com>
2024-07-22 21:52:02 +0200

    task-01 update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,48 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery')
+console.log(galleryEl)
+// create images elements
+
+function createImagesElements (items){
+    return items.map(item => `<li class="gallery-item">
+  <a class="gallery-link" href="large-image.jpg">
+    <img
+      class="gallery-image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</li>`)
+}
+
+function createImagesItems () {
+    galleryEl.innerHTML = createImagesElements(galleryItems).join('')
+
+
+}
+createImagesItems();
+
+
+//create modal window and click
+
+galleryEl.addEventListener('click', onImageClick)
+function onImageClick(el){
+    el.preventDefault();
+    if (el.target.classList.contains('gallery')){
+        return
+    }
+    const source = el.target.dataset.source;
+    const instance = basicLightbox.create(`
+        <img src="${source}" width="800" height="600">
+    `)
+    
+    instance.show()
+
+
+}
+
+
