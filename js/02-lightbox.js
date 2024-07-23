440349db3425a1ed87968f6fa83dd28f913a2a85
vbevz <122693446+vladbevz@users.noreply.github.com>
2024-07-23 21:33:02 +0200

    update task-02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryEL = document.querySelector('.gallery');
 
-console.log(galleryItems);
+function createImagesElements (items) {
+    return items.map(item => `<li class="gallery__item">
+   <a class="gallery__link" href="${item.original}">
+      <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+   </a>
+</li>`)
+
+}
+
+function createImagesItems(){
+    galleryEL.innerHTML = createImagesElements(galleryItems).join('');
+
+}
+
+createImagesItems();
+
+const lightbox = new SimpleLightbox(`.gallery a`, { 
+    captionsData: "alt",  
+    captionType: "alt",
+    captionDelay: 250,
+});
